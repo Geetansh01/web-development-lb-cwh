b68e6228cf617bb9921b4cc609a37302f4b66ee7
Geetansh <geetansh.bhardwaj04@gmail.com>
2023-12-19 00:26:42 +0530

    Tut60 Done

---
+++ Tut60/index.js
@@ -0,0 +1,20 @@
+console.log("String in JS");
+
+let my_name = "Geetansh";
+console.log(my_name);
+console.log(my_name[0]);
+console.log(my_name[1]);
+console.log(my_name[2]);
+console.log(my_name[7]);
+// console.log(my_name[8]);
+
+// let sentence = "This lion's "nature" is carnivorous by "birth !";  //Unable to write a string that contains double or single quotes {" ", ' '} in the usual manner 
+
+let sentence = `This lion's "nature" is carnivorous by "birth !`;  //Prblm solved with "Template literals" 
+console.log(sentence);
+
+let first_name = "Geetansh";
+let last_name = "Bhardwaj";
+console.log(`The name is ${first_name} ${last_name}`);
+
+
