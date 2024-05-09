847074fdea2f743e505ff8da6b3c9d77d033c671
Geetansh <145845798+Geetansh01@users.noreply.github.com>
2024-05-10 04:20:05 +0530

    Tut101 Twitter Clone Almost Done

---
+++ BackEnd/Tut101/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'twitterChirp': ['twitterChirp', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
