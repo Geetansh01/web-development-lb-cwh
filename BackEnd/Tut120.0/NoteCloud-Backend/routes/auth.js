52a6d4f6d8b3e7c1226f37651f3e443695dac361
Geetansh <145845798+Geetansh01@users.noreply.github.com>
2024-07-10 16:45:37 +0530

    Tut 46 Done

--- BackEnd/Tut120.0/NoteCloud-Backend/routes/auth.js
+++ BackEnd/Tut120.0/NoteCloud-Backend/routes/auth.js
@@ -1,19 +1,46 @@
 const express = require("express");
 const router = express.Router();
-const {User} = require("../models/User")
+const { User } = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 // define the home page route
 router.get("/", (req, res) => {
   res.json({
-    Desc: "Hit POST request here to register a user!"
+    Desc: "Hit POST request here to register a user!",
   });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const newUser = new User(req.body);
-  newUser.save();
-  res.send("Received!")
-})
+router.post(
+  "/",
+  [
+    body("name", "Err msg : Name Should not be Empty").isLength({ min: 1 }), //"name" should not be empty
+    body("email", "Err msg : Pls enter valid email").isEmail(), //verify valid email format
+    body("password", "Err msg : Password Should not be Empty").isLength({
+      min: 1,
+    }),
+  ],
+  (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      // console.log(req.body);
+      const newUser = new User(req.body);
+      newUser
+        .save()
+        .then(() => {
+          res.send("User creation request received!");
+        })
+        .catch((err) => {
+          console.log("\n" + "User Not Saved to DB\n");
+          console.log(err);
+          res.json({
+            Desc: "User Not Saved to DB",
+            Error: err.errmsg,
+          });
+        });
+    } else {
+      res.send({ errors: result.array() });
+    }
+  }
+);
 
 module.exports = router;
