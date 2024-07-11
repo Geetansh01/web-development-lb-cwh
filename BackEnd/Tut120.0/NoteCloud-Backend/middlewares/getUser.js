b992bcb64b87e51a6d41acafa40131c45fa90cb5
Geetansh <145845798+Geetansh01@users.noreply.github.com>
2024-07-11 13:52:40 +0530

    Tut 120.51 Done

---
+++ BackEnd/Tut120.0/NoteCloud-Backend/middlewares/getUser.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const JWT_SECRET_PHRASE = process.env.JWT_SECRET_PHRASE; //Get the "JWT_SECRET_PHRASE". Stored in "BackEnd\Tut120.0\NoteCloud-Backend\.env"
+
+let getUser = (req, res, next) => {
+  let authToken = req.header("auth-token"); //When i will make a request, i will include the jwt auth token in "auth-token" header
+
+  try {
+    let dataInJwtToken = jwt.verify(authToken, JWT_SECRET_PHRASE);
+
+    //Extract "user" from jwt token and add it to request object "req"
+    req.user = dataInJwtToken.user;
+
+    next();
+  } catch (err) {
+    res.status(401).send("Invalid Auth Token");
+  }
+};
+
+module.exports = {
+  getUser,
+};
--- BackEnd/Tut120.0/NoteCloud-Backend/routes/auth.js
+++ BackEnd/Tut120.0/NoteCloud-Backend/routes/auth.js
@@ -4,8 +4,10 @@ const { User } = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { getUser } = require("../middlewares/getUser")
+require('dotenv').config()
 
-const JWT_SECRET_PHRASE = "GeetanshJWTSecret@#";
+const JWT_SECRET_PHRASE = process.env.JWT_SECRET_PHRASE; //Get the "JWT_SECRET_PHRASE". Stored in "BackEnd\Tut120.0\NoteCloud-Backend\.env"
 
 // Home page route: GET
 router.get("/createuser", (req, res) => {
@@ -14,70 +16,72 @@ router.get("/createuser", (req, res) => {
   });
 });
 
-// Create user using POST request. No login required
-// router.post(
-//   "/createuser",
-//   [
-//     body("name", "Err msg : Name Should not be Empty").isLength({ min: 1 }), //"name" should not be empty
-//     body("email", "Err msg : Pls enter valid email").isEmail(), //verify valid email format
-//     body("password", "Err msg : Password Should not be Empty").isLength({
-//       min: 1,
-//     }),
-//   ],
-//   async (req, res) => {
-//     try {
-//       const result = validationResult(req);
-//       if (result.isEmpty()) {
-//         //If request passes express validator's validation
+// ROUTE 1 : Create user using: POST request on "/api/auth/createuser". No login required
+router.post(
+  "/createuser",
+  [
+    body("name", "Err msg : Name Should not be Empty").isLength({ min: 1 }), //"name" should not be empty
+    body("email", "Err msg : Pls enter valid email").isEmail(), //verify valid email format
+    body("password", "Err msg : Password Should not be Empty").isLength({
+      min: 1,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const result = validationResult(req);
+      if (result.isEmpty()) {
+        //If request passes express validator's validation
 
-//         const found = await User.findOne({ email: req.body.email });
-//         if (found) {
-//           //If user with this email already exists
-//           res.json({ Desc: "User with this email Already Exists!" });
-//           return;
-//         }
+        const found = await User.findOne({ email: req.body.email });
+        if (found) {
+          //If user with this email already exists
+          res.json({ Desc: "User with this email Already Exists!" });
+          return;
+        }
 
-//         //Generate Hash for password
-//         const password = req.body.password;
-//         const salt = await bcrypt.genSalt();
-//         const hash = await bcrypt.hash(password, salt);
+        //Generate Hash for password
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(password, salt);
 
-//         const newUser = await User.create({
-//           name: req.body.name,
-//           email: req.body.email,
-//           password: hash, //Store hash of password in the DB
-//         });
+        const newUser = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: hash, //Store hash of password in the DB
+        });
 
-//         //Send jwt authentication token to user
+        //Send jwt authentication token to user
 
-//         // Data to be sent in the JWT auth token to the user.
-//         // We will use this data to identify the user when they try to use the token to authenticate themselves in the future.
-//         // Sending user ID because the ID is indexed in our MongoDB.
-//         // Indexing means that MongoDB creates a data structure that improves the speed of data retrieval operations.
-//         // Querying based on an indexed field, like the user ID, is the fastest because it allows MongoDB to quickly locate the data without scanning the entire collection.
-//         const data2send = {
-//           user: {
-//             userID: newUser.id,
-//           },
-//         };
-//         const authToken = jwt.sign(data2send, JWT_SECRET_PHRASE);
+        // Data to be sent in the JWT auth token to the user.
+        // We will use this data to identify the user when they try to use the token to authenticate themselves in the future.
+        // Sending user ID because the ID is indexed in our MongoDB.
+        // Indexing means that MongoDB creates a data structure that improves the speed of data retrieval operations.
+        // Querying based on an indexed field, like the user ID, is the fastest because it allows MongoDB to quickly locate the data without scanning the entire collection.
+        const data2send = {
+          user: {
+            userID: newUser.id,
+          },
+        };
+        const authToken = jwt.sign(data2send, JWT_SECRET_PHRASE);
 
-//         return res.json({
-//           Desc: "User saved to DB",
-//           authToken: authToken,
-//         });
-//       } else {
-//         //If request fails validation (by express validator), return errors
-//         return res.status(400).send({ errors: result.array() });
-//       }
-//     } catch (error) {
-//       //Some entirely different internal error occurred
-//       return res.status(500).send("Some Error Occurred! It's Internal Server Error");
-//     }
-//   }
-// );
+        return res.json({
+          Desc: "User saved to DB",
+          authToken: authToken,
+        });
+      } else {
+        //If request fails validation (by express validator), return errors
+        return res.status(400).send({ errors: result.array() });
+      }
+    } catch (error) {
+      //Some entirely different internal error occurred
+      return res
+        .status(500)
+        .send("Some Error Occurred! It's Internal Server Error");
+    }
+  }
+);
 
-// Login user using POST request. No login required
+// ROUTE 2 : Login user using: POST "/api/auth/login". No login required
 router.post(
   "/login",
   [
@@ -108,7 +112,6 @@ router.post(
           return res.status(400).json({ Desc: "Incorrect Credentials" });
         }
 
-
         //User Verified, send jwt authentication token to user
 
         // Data to be sent in the JWT auth token to the user.
@@ -129,9 +132,26 @@ router.post(
       }
     } catch (error) {
       //Some entirely different internal error occurred
-      return res.status(500).send("Some Error Occurred! It's Internal Server Error");
+      return res
+        .status(500)
+        .send("Some Error Occurred! It's Internal Server Error");
     }
   }
 );
+
+// ROUTE 3 : Get logged in user details using: POST "/api/auth/getuser". Login required
+router.post("/getuser", getUser, async (req, res) => { 
+  try {
+    let id = req.user.userID; //////
+    let user = await User.findById(id).select("-password"); //Select all fields except the  password field
+    res.json(user);
+  } catch (error) {
+    //Some entirely different internal error occurred
+    console.log(error);
+    return res
+      .status(500)
+      .send("Some Error Occurred! It's Internal Server Error");
+  }
+});
 
 module.exports = router;
