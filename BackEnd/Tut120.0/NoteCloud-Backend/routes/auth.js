5e17c7bea219035426f0e6c4a381d3840445bc47
Geetansh <145845798+Geetansh01@users.noreply.github.com>
2024-07-11 11:09:24 +0530

    Tut 49 Done

--- BackEnd/Tut120.0/NoteCloud-Backend/routes/auth.js
+++ BackEnd/Tut120.0/NoteCloud-Backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs'); 
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET_PHRASE = "GeetanshJWTSecret@#"
 
 // Home page route: GET
 router.get("/createuser", (req, res) => {
@@ -32,11 +36,37 @@ router.post(
           res.json({ Desc: "User with this email Already Exists!" });
           return;
         }
+        
+        //Generate Hash for password
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(password, salt);
 
-        const newUser = await User.create(req.body);
+        const newUser = await User.create({
+          "name": req.body.name,
+          "email": req.body.email,
+          "password": hash //Store hash of password in the DB
+        });
+
+        //Send jwt authentication token to user
+
+        // Data to be sent in the JWT auth token to the user. 
+        // We will use this data to identify the user when they try to use the token to authenticate themselves in the future.
+        // Sending user ID because the ID is indexed in our MongoDB. 
+        // Indexing means that MongoDB creates a data structure that improves the speed of data retrieval operations. 
+        // Querying based on an indexed field, like the user ID, is the fastest because it allows MongoDB to quickly locate the data without scanning the entire collection.
+        const data2send = {
+          user: {
+            userID: newUser.id
+          }
+        }
+        const autToken = jwt.sign(data2send, JWT_SECRET_PHRASE);
+
+        console.log(newUser.id);
+
         res.json({
           Desc: "User saved to DB",
-          SavedUser: newUser,
+          authToken: autToken,
         });
       } else {
         //If request fails validation (by express validator), return errors
