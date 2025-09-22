dcaddb3c9c9e1eac56e9d491cd3931bf92dc360f
Geetansh <a@a.a>
2025-09-22 17:23:58 +0530

    fastAPI in progress

--- BackEnd/fastAPI/main.py
+++ BackEnd/fastAPI/main.py
@@ -1,19 +1,58 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from models import Product
+from database import session, engine
+from sqlalchemy.orm import Session
+import database_models
 
 app = FastAPI()
+
+database_models.Base.metadata.create_all(bind=engine) # This line tells SQLAlchemy that it has to make all the tables
+
+products = [Product(id=1, name="MacBook Air", description="Budget Laptop", price=99, quantity=10),
+            Product(id=2, name="MacBook Pro", description="Expensive Laptop", price=999, quantity=5),
+            Product(id=3, name="Lenovo Legion", description="Expensive Laptop", price=999, quantity=5),
+            Product(id=4, name="Lenovo Ideapad", description="Budget Laptop", price=999, quantity=5)
+            ]
+
+# Initialise the DB, so if it did not already had, populate it with above data
+def init_db():
+    db = session()
+
+    count = db.query(database_models.Product).count # Get number of rows in this table
+    
+    # Only insert data if the table is empty
+    if count == 0:        
+        for product in products:
+            db.add(database_models.Product(**(product.model_dump())))
+            '''
+                Description of what above line does:
+                (i) db.add(<requires an SQLAlchemy class's object i.e database_models.Product's object>): this is because it will add the "product" into the DB, so the "product" should be an object of the SQLAlchemy class that we have.
+                (ii) database_models.Product(<key-value-pairs>) : If given key-value pairs, this will make the required database_models.Product's object
+                (iii) "product".model_dump(): this gives a dictionary containing the key-value pair form of the "product" (here, "product" is a pydantic class's object and not SQLAlchemy class's object) 
+                (iv) "(iii)" above gives a dictionary but we need key-value pairs. **(<dictionary>) unpacks the <dictionary> into key-value pairs.
+            '''      
+        db.commit() 
+
+init_db() 
+
+def get_db():
+    db = session()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def greet():
     return "Welcome to my FastAPI backend"
 
-products = [Product(id=1, name="MacBook Air", description="Budget Laptop", price=99, quantity=10),
-            Product(id=2, name="MacBook Pro", description="Expensive Laptop", price=999, quantity=5),
-            ]
-
 @app.get("/products")
-def get_all_products():
-    return products
+def get_all_products(db: Session = Depends(get_db)):
+    # Query the DB
+    db_products = db.query(database_models.Product).all()
+    return db_products
 
 @app.get("/product/{id}")
 def get_all_products(id: int):
