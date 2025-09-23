b6a8ef12a69e8ae9d33bae8252164d8610d94d59
Geetansh <a@a.a>
2025-09-23 13:57:31 +0530

    fastapi done

--- BackEnd/fastAPI/main.py
+++ BackEnd/fastAPI/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from models import Product
 from database import session, engine
 from sqlalchemy.orm import Session
 import database_models
 
 app = FastAPI()
+
+# For CORS: add your frontend URL 
+# [Note that "frontend" is buggy right now, I have taken it from Telusko's github as is "https://github.com/navinreddy20/fastapi-demo/tree/products-with-ui"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:3000"],
+    allow_methods = ["*"]
+)
 
 database_models.Base.metadata.create_all(bind=engine) # This line tells SQLAlchemy that it has to make all the tables
 
@@ -35,6 +44,7 @@ def init_db():
 
 init_db() 
 
+# Function for dependency injection
 def get_db():
     db = session()
 
@@ -55,32 +65,40 @@ def get_all_products(db: Session = Depends(get_db)):
     return db_products
 
 @app.get("/product/{id}")
-def get_all_products(id: int):
-    for product in products:
-        if (product.id == id):
-            return product
+def get_all_products(id: int, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        return db_product
     return "Product not found"
 
 @app.post("/product")
-def add_product(product: Product):
-    products.append(product)
+def add_product(product: Product, db: Session = Depends(get_db)):
+    db.add(database_models.Product(**(product.model_dump())))
+    db.commit()
     return product # Echo back the product you added to the client
 
 # To update an already existing product's details
 @app.put("/products/{product_id}")
-def update_product(product_id: int, product: Product):
-    for i in range(len(products)):
-        if products[i].id == product_id:
-            products[i] = product
-            return {"message": "Product updated successfully", "product": product}
+def update_product(product_id: int, product: Product, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == product_id).first()
+    if db_product: 
+        # Since we don't know what has been updated, update all the fields except "id"
+        db_product.description = product.description
+        db_product.name = product.name
+        db_product.price = product.price
+        db_product.quantity = product.quantity
+
+        db.commit()
+        return {"message": "Product updated successfully", "product": product}
     return {"error": "Product not found"}
 
 @app.delete("/products/{product_id}")
-def delete_product(product_id: int):
-    for i in range(len(products)):
-        if products[i].id == product_id:
-            del products[i]
-            return {"message": "Product deleted successfully"}
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == product_id).first()
+    if db_product: 
+        db.delete(db_product)
+        db.commit()
+        return {"message": "Product deleted successfully"}
     return {"error": "Product not found"}
 
 
