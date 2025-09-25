342da32cd1773017acce1a7fa6a56a4ed6147f73
Geetansh <a@a.a>
2025-09-25 11:28:51 +0530

    tanstack in progress

--- FrontEnd/TanStackQuery/src/App.jsx
+++ FrontEnd/TanStackQuery/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "./components/Loading";
+import getTodosQueryOptions from "./queryOptions/todos_queryOptions";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Mostly same as "FrontEnd/TanStackQuery/src/App_1_Intro.jsx" but moved queryOptions to a separate file for better readability + Reusability (you can reuse getTodosQueryOptions() in some other component too!)
+  const { data, isPending, refetch } = useQuery(getTodosQueryOptions());
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{isPending ? <Loading /> : JSON.stringify(data.slice(0, 10))}</div>
+      <button onClick={refetch}> Refetch Data </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
