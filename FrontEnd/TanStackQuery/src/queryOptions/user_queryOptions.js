89ccf5ae53da253dc90051f5e8ee04c3f99b220f
Geetansh <a@a.a>
2025-11-17 17:21:05 +0530

    TSQ Done

--- FrontEnd/TanStackQuery/src/queryOptions/user_queryOptions.js
+++ FrontEnd/TanStackQuery/src/queryOptions/user_queryOptions.js
@@ -4,6 +4,28 @@ export default function getUserQueryOptions() {
   return queryOptions({
     queryKey: ["user"],
     queryFn: () => getUsers(),
+
+    // We have more options that we can use with useQuery hook
+
+    // 1) select: when we don't want the entire thing that "queryFn()" returns but only a subset of it. Now, query's "data" will have only that subset. ( See "src/App_3_MultipleQueries.jsx", here, "query1.data" will have only the fields returned by this "select" function )
+    select: (data) => {
+      // return data.<fields-you-want-to-return>;
+    },
+
+    // 2)
+    refetchInterval: 10000, // Refetch the data every 10 seconds (10000 milliseconds). Useful for data that changes frequently like stock prices.
+
+    // 3)
+    refetchOnWindowFocus: false, // By default, TanStack Query refetches the data when the window regains focus. Setting this to false disables that behavior.
+
+    // Note: staleTime VS refetchOnWindowFocus: staleTime will take precedence over refetchOnWindowFocus. If data is fresh (within staleTime), it will not refetch on window focus even if refetchOnWindowFocus is true. If you want "refetchOnWindowFocus" to take precedence, set: refetchOnWindowFocus: 'always'
+
+    // 4) 
+    // placeholderData: [], // While the query is loading for the first time, "data" will have this placeholder data. Once the data is fetched, "data" will have the actual data. Useful for avoiding "undefined" states in your UI.
+
+    // 5)
+    // initialData: [], // Similar to placeholderData but different in one key aspect: It is actually cached in the TanStack Query cache. So, if you have staleTime set to say 5s, and you navigate away from the component and come back within 5s, the initialData is still fresh in the cache and won't trigger a refetch. With placeholderData, it is not cached, so navigating away and back will cause a refetch every time.
+
   });
 
   /*
