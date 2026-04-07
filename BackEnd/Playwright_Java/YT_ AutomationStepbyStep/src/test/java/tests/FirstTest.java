d961fd58ea07cdc7c422d50f9244e94b0c31d278
Geetansh <145845798+Geetansh01@users.noreply.github.com>
2026-04-08 00:03:24 +0530

    playwright in progress

---
+++ BackEnd/Playwright_Java/YT_ AutomationStepbyStep/src/test/java/tests/FirstTest.java
@@ -0,0 +1,31 @@
+package tests;
+import base.BaseTest;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.testng.annotations.Test;
+
+public class FirstTest extends BaseTest {
+
+    @Test
+    public void verifyTitle(){
+        page.navigate("https://google.com/ncr");
+
+        // Optional: Handle cookie popup
+        if(page.isVisible("button:has-text('Accept all')")){
+            page.click("button:has-text('Accept all')");
+        }
+        System.out.println("Page Title: " + page.title());
+    }
+
+//    public static void main(String[] args) {
+//        try(Playwright playwright = Playwright.create()){
+//            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+//            Page page = browser.newPage();
+//            page.navigate("https://google.com");
+//            System.out.println("Page Title: " + page.title());
+//            browser.close();
+//        }
+//    }
+}
